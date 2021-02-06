42add1198e7519a3bbaab47add3c6d5c8babb2f0
nyebra <78374139+nyebra@users.noreply.github.com>
2021-02-07 00:31:32 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mi-primera-app-angular';
+  nombre = 'esta es mi primera clase de Angular';
+}
